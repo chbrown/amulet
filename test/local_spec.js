5da0af765d57d9a8a59a31a8301bf9e109781a1c
Christopher Brown <io@henrian.com>
2013-06-03 17:30:00 -0500

    Cleanup local spec

--- test/local_spec.js
+++ test/local_spec.js
@@ -3,10 +3,6 @@ var fs = require('fs');
 var yaml = require('js-yaml');
 var amulet = require('../lib/rendering');
 var test = require('tap').test;
-var argv = require('optimist').default({
-  'ignore-whitespace': true,
-  extended: false
-}).argv;
 
 function extendString() {
   String.prototype.capitalize = function() {
@@ -14,11 +10,11 @@ function extendString() {
   };
   String.prototype.titleize = function() {
     var result = [];
-    var parts = this.split(" ");
+    var parts = this.split(' ');
     for (var ii in parts) {
       result.push(parts[ii].capitalize());
     }
-    return result.join(" ");
+    return result.join(' ');
   };
   String.prototype.humanize = function() {
     return this.replace(/_/g, ' ').titleize();
@@ -30,14 +26,16 @@ function extendString() {
 
 function testSpec(t, spec) {
   // console.error(spec.description);
-  // parse template into the saved template name that is the description, e.g. "boolean"
+  // parse template into the saved template name that is the description, e.g. 'boolean'
   amulet.parseTemplate(spec.description, spec.template);
 
   var context = eval('(' + spec.context + ')');
   var expected = spec.output;
   // render with that same name, too.
   amulet.renderString(spec.description, context, function(err, output) {
-    if (argv['ignore-whitespace']) {
+    // have to read the whitespace arg from the environment, not from the command line
+    if (process.env.WHITESPACE && !process.env.WHITESPACE.match(/false/i)) {
+      console.error('Ignoring whitespace.');
       output = output.replace(/\s+/g, '');
       expected = expected.replace(/\s+/g, '');
     }
